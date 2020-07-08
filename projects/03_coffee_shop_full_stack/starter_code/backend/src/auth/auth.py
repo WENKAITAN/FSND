45ea0f36d7e581c709091314bcc7313723b24532
WENKAI TAN <52226880+WenkaiTan@users.noreply.github.com>
2020-07-08 18:40:29 -0400

    2

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -7,7 +7,7 @@ from urllib.request import urlopen
 
 AUTH0_DOMAIN = 'tancoffee.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'coffeeshop'
+API_AUDIENCE = 'http://localhost:5000'
 
 ## AuthError Exception
 '''
--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
     url: 'tancoffee.us.auth0.com', // the auth0 domain prefix
-    audience: 'coffeeshop', // the audience set for the auth0 app
-    clientId: 'DI7gApu3ygo7YuIb04KHPe4SdAJws1ze', // the client id generated for the auth0 app
+    audience: 'http://localhost:5000', // the audience set for the auth0 app
+    clientId: 'ugjBVfr9Ah4jb5HWMQklIaBBG8x52o6z', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
